92d29d4c4d558b9798102b79c591a501e9cc5452
Süleyman Tolga Acar <tolgasuleymanacar@gmail.com>
2023-11-05 20:22:27 +0300

    finished main functionality

--- src/App.java
+++ src/App.java
@@ -1,12 +1,42 @@
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        Tree tree = new Tree(new Node("A", 5.0));
-        tree.insert("B", 2.0);
-        tree.insert("F", 6.0);
-        tree.insert("D", 4.0);
-        tree.insert("E", 5.0);
-        tree.insert("C", 3.0);
-        tree.insert("G", 7.0);
-        tree.print();
+        File inputFile = new File(args[0]);
+        Scanner scanner = new Scanner(inputFile);
+        PrintWriter writer = new PrintWriter(args[1], "UTF-8");
+        String[] boss = scanner.nextLine().split(" ");
+        Tree tree = new Tree(new Node(boss[0], Double.parseDouble(boss[1])), writer);
+
+        while (scanner.hasNextLine()) {
+            String[] line = scanner.nextLine().split(" ");
+            String operation = line[0];
+
+            switch (operation) {
+                case "MEMBER_IN":
+                    tree.insert(line[1], Double.parseDouble(line[2]));
+                    break;
+                case "MEMBER_OUT":
+                    tree.remove(Double.parseDouble(line[2]));
+                    break;
+                case "INTEL_TARGET":
+                    Node ancestor = tree.lowestCommonAncestor(Double.parseDouble(line[2]), Double.parseDouble(line[4]));
+                    writer.println("Target Analysis Result: " + ancestor.name + " " + String.format(Locale.US, "%.3f", ancestor.value));
+                    break;
+                case "INTEL_DIVIDE":
+                    int maxCount = tree.maxIndependentNodes();
+                    writer.println("Division Analysis Result: " + maxCount);
+                    break;
+                case "INTEL_RANK":
+                    tree.nodesWithSameRank(Double.parseDouble(line[2]));
+                    break;
+            }
+        }
+
+        scanner.close();
+        writer.close();
     }
 }
--- src/Tree.java
+++ src/Tree.java
@@ -1,18 +1,28 @@
+import java.io.PrintWriter;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Locale;
+
 public class Tree {
     public Node root;
+    public PrintWriter writer;
 
-    public Tree(Node root) {
+    public Tree(Node root, PrintWriter writer) {
         this.root = root;
+        this.writer = writer;
     }
 
     public void print(){
+        System.out.println("-------------------------");
         print(root);
+        System.out.println("-------------------------");
     }
 
     private void print(Node node) {
         if (node == null) return;
         print(node.left);
-        System.out.println(node.name + " " + node.value);
+        System.out.println(node.name + " " + String.format(Locale.US, "%.3f", node.value));
         print(node.right);
     }
 
@@ -25,10 +35,14 @@ public class Tree {
             return new Node(name, value);
         }
 
+        writer.println(node.name + " welcomed " + name);
+
         if (value < node.value) {
             node.left = insert(node.left, name, value);
+            node.left.parent = node;
         } else if (value > node.value) {
             node.right = insert(node.right, name, value);
+            node.right.parent = node;
         }
 
         return balance(node);
@@ -43,29 +57,35 @@ public class Tree {
     }
 
     public Node remove(double value) {
-        return remove(root, value);
+        return remove(root, value, false);
     }
 
-    private Node remove(Node node, double value) {
+    private Node remove(Node node, double value, boolean removed) {
         if (node == null) {
             return null;
         }
 
         if (value < node.value) {
-            node.left = remove(node.left, value);
+            node.left = remove(node.left, value, removed);
         } else if (value > node.value) {
-            node.right = remove(node.right, value);
+            node.right = remove(node.right, value, removed);
         } else {
-            if (node.left == null) {
+            if (node.left == null && node.right == null) {
+                if (!removed) writer.println(node.name + " left the family, replaced by nobody");
+                return null;
+            } else if (node.left == null) {
+                if (!removed) writer.println(node.name + " left the family, replaced by " + node.right.name);
                 return node.right;
             } else if (node.right == null) {
+                if (!removed) writer.println(node.name + " left the family, replaced by " + node.left.name);
                 return node.left;
             }
 
             Node min = findMin(node.right);
+            writer.println(node.name + " left the family, replaced by " + min.name);
             node.name = min.name;
             node.value = min.value;
-            node.right = remove(node.right, min.value);
+            node.right = remove(node.right, min.value, true);
         }
 
         return balance(node);
@@ -77,6 +97,9 @@ public class Tree {
         right.left = node;
         node.height = Math.max(height(node.left), height(node.right)) + 1;
         right.height = Math.max(height(right.left), height(right.right)) + 1;
+        if(node == root) root = right;
+        right.parent = node.parent;
+        node.parent = right;
         return right;
     }
 
@@ -86,6 +109,9 @@ public class Tree {
         left.right = node;
         node.height = Math.max(height(node.left), height(node.right)) + 1;
         left.height = Math.max(height(left.left), height(left.right)) + 1;
+        if(node == root) root = left;
+        left.parent = node.parent;
+        node.parent = left;
         return left;
     }
 
@@ -124,4 +150,74 @@ public class Tree {
         node.height = Math.max(height(node.left), height(node.right)) + 1;
         return node;
     }
+
+    public Node lowestCommonAncestor(double value1, double value2) {
+        return lowestCommonAncestor(root, value1, value2);
+    }
+
+    private Node lowestCommonAncestor(Node node, double value1, double value2) {
+        if (node == null || node.value == value1 || node.value == value2) {
+            return node;
+        }
+
+        Node left = lowestCommonAncestor(node.left, value1, value2);
+        Node right = lowestCommonAncestor(node.right, value1, value2);
+
+        if (left != null && right != null) {
+            return node;
+        }
+
+        return (left != null) ? left : right;
+    }
+
+    public void nodesWithSameRank(double value) {
+        nodesWithSameRank(root, value);
+    }
+
+    private void nodesWithSameRank(Node node, double value) {
+        LinkedList<Node> queue = new LinkedList<Node>();
+        ArrayList<Node> nodes = new ArrayList<Node>();
+
+        queue.offer(node);
+        boolean found = false;
+        while(!queue.isEmpty()) {
+            nodes.clear();
+            int size = queue.size();
+            for(int i = 0; i < size; i++) {
+                Node current = queue.poll();
+                nodes.add(current);
+                if(current.value == value) found = true;
+                if(current.left != null) queue.offer(current.left);
+                if(current.right != null) queue.offer(current.right);
+            }
+            if(found) break;
+        }
+
+        writer.print("Rank Analysis Result:");
+        for(Node n : nodes) {
+            writer.print(" " + n.name + " " + String.format(Locale.US, "%.3f", n.value));
+        }
+        writer.println();
+    }
+
+    public int maxIndependentNodes() {
+        return maxIndependentNodes(root);
+    }
+
+    private int maxIndependentNodes(Node node) {
+        if (node == null) return 0;
+
+        int maxWithRoot = 1;
+        if (node.left != null) {
+            maxWithRoot += maxIndependentNodes(node.left.left) + maxIndependentNodes(node.left.right);
+        }
+        if (node.right != null) {
+            maxWithRoot += maxIndependentNodes(node.right.left) + maxIndependentNodes(node.right.right);
+        }
+
+        int maxWithoutRoot = maxIndependentNodes(node.left) + maxIndependentNodes(node.right);
+        
+        return Math.max(maxWithRoot, maxWithoutRoot);
+    }
 }
+    
